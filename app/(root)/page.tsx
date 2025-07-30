be41e132df1a46a84889d24a7af7463b2d1b4e5a
daniel <daniellimafreita@gmail.com>
2025-07-30 20:58:13 -0300

    :sparkles: feat: Developing search products component to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
+import ViewAllProductsButton from "@/components/view-all-products-button";
 
 export const metadata = {
   title: "Home",
@@ -21,6 +22,7 @@ const  HomePage = async () => {
       title="Newest Arrivals"
       limit={6}
     />  
+    <ViewAllProductsButton/>
   </>
 }
 
